65f4b21c4bc14aabce0180f6d451ad49e8369840
jcwaldron <jessica.c.waldron@gmail.com>
2024-10-18 20:34:07 -0500

    changed contrast level for light mode

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Hi there! You've found the repository for my portfolio site and business card. I
 # Hi, I'm Jessica!👋
 I'm a full stack developer with a broad background of experience that informs the work I do as a developer. I'm enthusiastic about technology that creates accessibility and helps amazing people do their amazing work.
 
-If you'd like to connect with me on LinkedIn, I'd be delighted!
+If you'd like to connect with me on LinkedIn, I'd be delighted! Drop me a note that you found me via GitHub!
 
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/jessica-c-waldron)
--- script.js
+++ script.js
@@ -2,23 +2,83 @@
 
 function toggleDarkMode() {
     var bodyElement = document.querySelector('body');
-    var buttonElement = document.querySelector('button'); 
+    var headerElement = document.querySelector('header');
 
-    // Toggle the 'dark-mode' class on the body element
-    if (bodyElement) {
+    // Ensure 'nineties-mode' is removed before toggling 'dark-mode'
+    if (bodyElement.classList.contains('nineties-mode')) {
+        bodyElement.classList.remove('nineties-mode');
+    }   
+
+     // Toggle the 'dark-mode' class on the body element
         bodyElement.classList.toggle('dark-mode');
-        
+        headerElement.classList.remove('nineties-mode')
+        darkSwitch();
+        ninetiesSwitch();
+    }
+
+function darkSwitch(){
+    var bodyElement = document.querySelector('body');
+    var darkButton = document.getElementsByClassName('dark-button')[0]; 
+
         // Update button text based on whether dark mode is active
         if (bodyElement.classList.contains('dark-mode')) {
-            buttonElement.textContent = "☀️ Light Mode";
-            buttonElement.style.backgroundColor = "#66999B"
-            buttonElement.style.color = "black";
-            buttonElement.style.fontWeight = "bold";
+            darkButton.textContent = "☀️ Light Mode";
+            darkButton.style.backgroundColor = "#86c6c9"
+            darkButton.style.color = "black";
+            darkButton.style.fontWeight = "bold";
         } else {
-            buttonElement.textContent = "🌙 Dark Mode";
-            buttonElement.style.backgroundColor = "#375253";
-            buttonElement.style.color = "white";
-            buttonElement.style.fontWeight = "normal";
+            darkButton.textContent = "🌙 Dark Mode";
+            darkButton.style.backgroundColor = "#375253";
+            darkButton.style.color = "white";
+            darkButton.style.fontWeight = "normal";
         }
+}
+
+// toggle for 90s mode
+function toggle90sMode(){
+    var bodyElement = document.querySelector('body');
+    // var headerElement = document.querySelector('header');
+    // var navElement = document.querySelector('nav');
+    // var linksElement = document.getElementById('links');
+    // var togglesElement = document.getElementById('toggles');
+    // var footerElement = document.querySelector('footer');
+    // var footerDiv = document.getElementById('footerdiv');
+    // var footerP = document.getElementById('footerp');
+
+    // Ensure 'dark-mode' is removed before toggling 'nineties-mode'
+    if (bodyElement.classList.contains('dark-mode')) {
+        bodyElement.classList.remove('dark-mode');
+    }   
+    
+    // Toggle the 'dark-mode' class on the body element
+    bodyElement.classList.toggle('nineties-mode');
+    // headerElement.classList.toggle('nineties-mode');
+    // navElement.classList.toggle('nineties-mode');
+    // linksElement.classList.toggle('nineties-mode');
+    // togglesElement.classList.toggle('nineties-mode');
+    // footerElement.classList.toggle('nineties-mode');
+    // footerDiv.classList.toggle('nineties-mode');
+    // footerP.classList.toggle('nineties-mode')
+    ninetiesSwitch();
+    darkSwitch();
+    
     }
+
+function ninetiesSwitch(){
+    var bodyElement = document.querySelector('body');
+    var ninetiesButton = document.getElementsByClassName('nineties-button')[0];    
+
+        // Update button text based on whether dark mode is active
+        if (bodyElement.classList.contains('nineties-mode')) {
+            ninetiesButton.textContent = "❌ AHH, UNDO!";
+            ninetiesButton.style.backgroundColor = "blue"
+            ninetiesButton.style.color = "white";
+            ninetiesButton.style.fontWeight = "bold";
+        } else {
+            ninetiesButton.textContent = "🌐 '90s Mode";
+            ninetiesButton.style.backgroundColor = "#375253";
+            ninetiesButton.style.color = "white";
+            ninetiesButton.style.fontWeight = "normal";
+            }
+        
 }
